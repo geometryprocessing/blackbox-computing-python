63013f9309c8afaf41b69823fab414b24db6517a
Sebastian Koch <s.koch@phusion.eu>
2020-05-22 18:42:04 +0200

    Cleaup old files, fix several problems, add new binder links, ...

--- doc/plot_to_md.py
+++ doc/plot_to_md.py
@@ -1,5 +1,7 @@
 import meshplot
 import json
+#import plotly.offline as plotly
+#import plotly.graph_objects as go
 
 first = True
 meshplot.website()
@@ -13,6 +15,15 @@ def mp_to_md(self):
         res = self.to_html(imports=False, html_frame=False)
 
     return res
+    
+#def plotly_to_md(self):
+#    print("PLOTLING")
+#    html = '<script src="https://cdn.plot.ly/plotly-latest.min.js"></script>'
+#    html += "<div>"
+#    html += plotly.plot(self, output_type='div', include_plotlyjs=first, show_link=False)
+#    html += "</div>"
+
+#    return html
 
 def sp_to_md(self):
     global first
@@ -32,4 +43,13 @@ def lis_to_md(self):
     return res
 
 get_ipython().display_formatter.formatters["text/html"].for_type(meshplot.Viewer, mp_to_md)
+#get_ipython().display_formatter.formatters["text/html"].for_type(go.Figure, plotly_to_md)
 #get_ipython().display_formatter.formatters["text/html"].for_type(meshplot.Subplot, sp_to_md)
+
+
+
+
+
+
+
+
